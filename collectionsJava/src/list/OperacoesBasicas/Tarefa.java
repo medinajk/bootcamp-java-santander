fb0b88850608274dae890af3b745daf5b3eae66e
Bianca Medina <122565445+medinajk@users.noreply.github.com>
2024-07-10 14:45:53 -0300

    Exercícios sobre List

---
+++ collectionsJava/src/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,21 @@
+package list.OperacoesBasicas;
+
+public class Tarefa {
+    //atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa{" +
+                "descricao='" + descricao + '\'' +
+                '}';
+    }
+}
